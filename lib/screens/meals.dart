f70bb30803ecafb1a0972a893799cc0cd0e02921
Shashank Kumar Srivastava <srivastavashashank033@gmail.com>
2023-08-06 01:24:26 +0530

    meta-data-added

--- lib/screens/meals.dart
+++ lib/screens/meals.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:meals_app/widgets/meal_item.dart';
 
 import '../models/meal.dart';
 
@@ -9,28 +10,32 @@ class MealsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Widget content = Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-             Text("Nothing's here",style: Theme.of(context).textTheme.headlineLarge!.copyWith(
-                    color: Theme.of(context).colorScheme.onBackground),
-            ),
-            Text(
-              "Try selecting a different category!!!",
-              style: Theme.of(context).textTheme.bodyLarge!.copyWith(
-                    color: Theme.of(context).colorScheme.onBackground,
-                  ),
-            ),
-          ],
-        ),
-      ); 
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Text(
+            "Nothing's here",
+            style: Theme.of(context)
+                .textTheme
+                .headlineLarge!
+                .copyWith(color: Theme.of(context).colorScheme.onBackground),
+          ),
+          Text(
+            "Try selecting a different category!!!",
+            style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                  color: Theme.of(context).colorScheme.onBackground,
+                ),
+          ),
+        ],
+      ),
+    );
     if (meals.isNotEmpty) {
       content = ListView.builder(
         itemCount: meals.length,
-      itemBuilder: (context, index) => Text(
-        meals[index].title,
-      ),
-    );
+        itemBuilder: (context, index) => MealItem(
+          meal: meals[index],
+        ),
+      );
     }
     return Scaffold(
       appBar: AppBar(
