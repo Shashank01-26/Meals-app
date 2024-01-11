5e4e233e8d6674d92c9276b263e2110a420e6be0
Shashank Kumar Srivastava <srivastavashashank033@gmail.com>
2024-01-12 00:21:17 +0530

    Implicit animation for favorites

--- lib/screens/meal_detail.dart
+++ lib/screens/meal_detail.dart
@@ -21,7 +21,26 @@ class MealDetailScreen extends ConsumerWidget {
         title: Text(meal.title),
         actions: [
           IconButton(
-            icon: Icon(isFavorite ? Icons.star : Icons.star_border),
+            icon: AnimatedSwitcher(
+                duration: const Duration(milliseconds: 300),
+                transitionBuilder: (child, animation){
+                  return RotationTransition(
+                      turns: Tween(
+                        begin: 0.9,
+                        end: 1.0
+                      ).animate(animation),
+                      child: child,
+                  );
+                },
+                child: Icon(
+                    isFavorite
+                        ?
+                    Icons.star
+                        :
+                    Icons.star_border,
+                  key: ValueKey(isFavorite),
+                ),
+            ),
             onPressed: () {
              final wasAdded = ref
                   .read(favoriteMealsProvider.notifier)
